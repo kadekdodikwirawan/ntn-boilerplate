e13caa684f50694e416ac88f1f9088c665fb5a0f
dodikdogen <dodikdogen@gmail.com>
2024-06-13 03:32:25 +0000

    update homepage and siteinfo

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
-      <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
-      <h2 class="subtitle text-center">Boilerplate</h2>
+      <h1 class="title text-center">Kadek Dodik Wirawan</h1>
+      <h2 class="subtitle text-center">Fullstack Web Developer</h2>
+      <p class="text-center">Hello! I’m Kadek Dodik Wirawan, a dedicated professional with a passion for excellence in website development and server management, live in Pemuteran, Buleleng, Bali, Indonesia.</p>
+      <img src="https://kadekdodikwirawan.github.io/assets/img/hero-bg.JPG" class="cover-image">
     </section>
 
     <section class="mt-8">
